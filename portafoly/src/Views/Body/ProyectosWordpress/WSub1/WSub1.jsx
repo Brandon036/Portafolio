f4d12b4129ef7c8c8f869e0546915f3612fab2bf
Camilo <bcbjpro036crak@gmail.com>
2023-11-11 11:28:54 -0500

    w1

--- portafoly/src/Views/Body/ProyectosWordpress/WSub1/WSub1.jsx
+++ portafoly/src/Views/Body/ProyectosWordpress/WSub1/WSub1.jsx
@@ -1,25 +1,37 @@
 import React from 'react'
 import style from "./WSub.module.css"
+/*imagenes de manera importada */
+import img1 from "./Wimgs/w1Basic1.webp"
+import img2 from "./Wimgs/w1Basic2.webp"
+import img3 from "./Wimgs/w1Basic3.webp"
+import img4 from "./Wimgs/w1Basic4.webp"
+import img5 from "./Wimgs/w1Basic5.webp"
+
+
+
+
 function WSub1() {
   return (
-    <div>
-<h1>Pagina de Inicio</h1>
-<h2> Logo, Cabezera, Menu, Titulo, Descripcion, video final, footer, Whatsapp.</h2>
+    <div className={style.Wsub1}>
+<h1 className={style.t}>Pagina de Inicio</h1>
+<h2 className={style.t}> Logo, Cabezera, Menu, Titulo, Descripcion,Servicios, Video final, footer, Whatsapp.</h2>
 
-<div className={style.columnWimg}>
-        <div className={style.WSub1img}/>
-        <div  className={style.WSub1img2}/>
-        <div className={style.WSub1img3}/>
-        <div className={style.WSub1img4}/>
-</div>
-<h2> ghfvgfhg </h2>
+    <section>
+      <img className={style.imgG} src={img1} alt="Logo, Cabezera, Menu" />
+      <img className={style.imgG} src={img2} alt="Titulo, Descripcion" />
+      <img className={style.imgG} src={img3} alt="Servicios" />
+      <img className={style.imgG} src={img4} alt="video final, footer, Whatsapp." />
+      <img className={style.imgG1} src={img5} alt=""/>
+    </section>
+    <br/>
+<a href="/">
+  <button className={style.botonBr}>
+    Devolver
+    </button>
+</a>
 
-<div className={style.columnWimg1}>
-        <div className={style.WSu1img5}/>
-        <div  className={style.WSub1img6}/> 
-      
-</div>
     
+
     </div>
   )
 }
